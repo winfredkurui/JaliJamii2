898c1c3e4f68d95ffd1144ddf875b3f28f5a0b18
winfredkurui <winfredkurui@gmail.com>
2018-03-01 16:30:08 +0300

    new files

---
+++ app/src/main/java/com/example/winfredkurui/jalijamii/DonationTypeActivity.java
@@ -0,0 +1,13 @@
+package com.example.winfredkurui.jalijamii;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DonationTypeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_donation_type);
+    }
+}
